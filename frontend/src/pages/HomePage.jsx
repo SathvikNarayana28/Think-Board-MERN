b65b050dadae24b0cb1e7563421354dc38de7df4
SathvikNarayana49 <sathviknarayana49@gmail.com>
2025-08-09 23:20:19 +0530

    case sensitivity

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 import api from "../lib/axios";
 import toast from "react-hot-toast";
 import NoteCard from "../Components/NoteCard";
-import NotesNotFound from "../components/NotesNotFound";
+import NotesNotFound from "../Components/NotesNotFound";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
